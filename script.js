0777e83c90ea485e8eebd253ce72f7d9076088d8
astridev <abostridev@gmail.com>
2025-12-12 21:22:02 +0100

    Add getRandomNumber

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hey')
+console.log('Hey');
+
+
+const randomNumber= document.querySelector('#random-number');
+randomNumber.addEventListener('click', ()=> {
+    document.querySelector("#result").textContent = getRandomNumber(1,100);
+})
+
+function getRandomNumber(min,max) {
+    return Math.random() * (max - min) + min;
+}
