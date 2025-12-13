c8829a0720cd916bf776f4d0ee5ce8f62faa0d7d
astridev <abostridev@gmail.com>
2025-12-13 15:55:25 +0100

    update script.js to include min max

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const randomNumber= document.querySelector('#random-number');
-randomNumber.addEventListener('click', ()=> {
-    document.querySelector("#result").textContent = getRandomNumber(1,100);
+const randomNumber = document.querySelector('#random-number');
+const result = document.querySelector('#result');
+const min  = document.querySelector('#min');
+const max  = document.querySelector('#max');
+
+randomNumber.addEventListener('click', () => {
+    result.textContent = Math.floor(
+        getRandomNumber(Number(min.value), Number(max.value))
+    );
 })
 
-function getRandomNumber(min,max) {
+function getRandomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
